3a1590cea79729710c8b8b38e52c7e992ed17ab6
Alex Pelavin <ajpele@gmail.com>
2016-07-28 19:28:59 +0000

    Cleaned up some incorrect namespacing

--- lib/kinesis.rb
+++ lib/kinesis.rb
@@ -1,11 +1,10 @@
 require_relative '../../resource/lib/resource.rb'
-class Resource
-  class Kinesis < self
-    require_relative 'kinesis/stream'
 
-    def initialize(*args)
-      super(*args)
-      @aws_client = Aws::Kinesis::Client.new(region: @desired_properties[:region])
-    end
+class Kinesis < Resource
+  require_relative 'kinesis/stream'
+
+  def initialize(*args)
+    super(*args)
+    @aws_client = Aws::Kinesis::Client.new(region: @desired_properties[:region])
   end
 end
